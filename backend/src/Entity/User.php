9d6de7978d0db415a671bec96c9e8c5497281076
thibaultverlain <thib.vintedpro@gmail.com>
2026-04-30 11:26:59 +0200

    fix: search user

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -39,6 +39,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private Collection $projects;
 
     #[ORM\ManyToMany(targetEntity: Task::class, inversedBy: 'users')]
+    #[ORM\JoinTable(name: 'user_task')]
     private Collection $tasks;
 
     #[ORM\Column]
